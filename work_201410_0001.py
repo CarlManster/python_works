124a5c89b3b8337c90c9cd5982f5d8cfcd03fa90
최종민 <manster@tmon.co.kr>
2014-10-22 19:03:45 +0900

    # printLoop : iterable 타입 검사 방식 변경
    # reduceComparedRange
    : 조건 변경
    
    # 복소수 예제 추가

--- work_201410_0001.py
+++ work_201410_0001.py
@@ -1,4 +1,4 @@
-import math
+import math, types
 
 def drawLine(): print("--------------------")
 	
@@ -12,20 +12,27 @@ def showNum(a, b):
 	drawLine()
 	
 def printLoop(a):
-	if type(a) is not str and type(a) is not dict and type(a) is not list:
-		print('%s (0)o' % (a))
-	else :
+	from types import *
+	try:
 		for x in a:
-			if type(a) is dict: print('%s (%s)v' % (x, a[x]))
-			elif type(x) is str: print('%s (%s)c' % (x, len(x))) 
-			elif type(x) is int: print('%s (%s)i' % (x, intLen(x)))
-			else: print(x)
+			if type(a) is DictType: print('%s (%s)d' % (x, a[x]))
+			elif type(x) is StringType: print('%s (%s)s' % (x, len(x))) 
+			elif type(x) is ComplexType: n,m = compLen(x); print('%s (%s, %s)c' % (x, n, m))
+			elif type(x) in [BooleanType, IntType, LongType, FloatType]: print('%s (%s)i' % (x, intLen(x)))
+			else: print('%s (0)u' % (x))
+	except TypeError:
+		print('%s (0)o' % (a))
 	drawLine()
 			
 def intLen(a):
 	digit = 1.0
 	if a != 0: digit = math.floor(math.log10(abs(a))) + 1
 	return int(digit)
+	
+def compLen(a):
+	digitReal = intLen(a.real)
+	digitImag = intLen(a.imag)
+	return digitReal, digitImag
 
 def variTest1():
 	print("variTest1")
@@ -43,7 +50,7 @@ def gender(x):
 	if x:
 		return "Male"
 	return "Female"
-	
+
 def mapSquaredRange(x, y):
 	return map(lambda z : z ** 2, range(x, y))
 	
@@ -51,7 +58,7 @@ def mapSquaredRangeFromZero(x):
 	return mapSquaredRange(0, x)
 	
 def reduceComparedRange(x, y):
-	return reduce(lambda a, b : a if a > b and a % 3 == 0 else b, range(x, y))
+	return reduce(lambda a, b : a if a > b and a % 3 == 0 else a + b, range(x, y))
 
 def reduceComparedRangeFromZero(x):
 	return reduceComparedRange(0, x)
@@ -61,9 +68,23 @@ def filterOddRange(x, y):
 	
 def filterOddRangeFromZero(x):
 	return filterOddRange(0, x)
+	
+def rangef(x, y, jmp=1.0):
+	result = []
+	while x < y:
+		result.append(x)
+		x = x + jmp
+	return result
 
 def makeComplex(a, b):
 	return complex(a+b*1j)
+	
+def makeComplexRange(x1, y1, x2, y2):
+	result = []	
+	for n in rangef(x1, y1):
+		for m in rangef(x2, y2):
+			result.append(makeComplex(n, m))
+	return result
 
 comp1 = complex(10.0+7.2j)
 comp2 = complex(2.0+2.6j)
@@ -71,6 +92,8 @@ comp3 = makeComplex(1.7, 2.9)
 family = ["grand father", "grand mother", "father", "mother", "son", "daughter"]
 person = {"name" : "Carl", "age" : 33, "male" : True, "lat" : 38.537798, "lon" : 126.835636}
 numbers = range(-120, 120, 37)
+floats = rangef(-12.03, 20.97, 2.21)
+comps = makeComplexRange(9, 15, 2, 7)
 
 drawLine()
 
@@ -81,6 +104,7 @@ print('%s + %s = %s' % (comp1, comp2, comp1 + comp2))
 print('%s + %s = %s' % (comp1, comp3, comp1 + comp3))
 print(numbers)
 print(person)
+print(comps)
 
 drawLine()
 
@@ -90,6 +114,8 @@ variTest2()
 
 printLoop(family)
 printLoop(numbers)
+printLoop(floats)
+printLoop(comps)
 printLoop(person)
 printLoop("Hello World")
 printLoop(mapSquaredRangeFromZero(15))
